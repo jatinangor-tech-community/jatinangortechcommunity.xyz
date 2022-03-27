761eddba10add036e6ef368f836f1140d4c7f274
Risyandi <risyandi.94@gmail.com>
2022-03-27 21:27:41 +0700

    fix: update map js latlong and logo

--- web/assets/vendor/map.js
+++ web/assets/vendor/map.js
@@ -3,13 +3,87 @@
 function initMap() {
 
   // Specify features and elements to define styles.
-  var styleArray = 
+  var styleArray =
+    // Begin map style: https://snazzymaps.com/
+    [{
+      "featureType": "landscape",
+      "stylers": [{
+        "saturation": -100
+      }, {
+        "lightness": 65
+      }, {
+        "visibility": "on"
+      }]
+    }, {
+      "featureType": "poi",
+      "stylers": [{
+        "saturation": -100
+      }, {
+        "lightness": 51
+      }, {
+        "visibility": "simplified"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "stylers": [{
+        "saturation": -100
+      }, {
+        "visibility": "simplified"
+      }]
+    }, {
+      "featureType": "road.arterial",
+      "stylers": [{
+        "saturation": -100
+      }, {
+        "lightness": 30
+      }, {
+        "visibility": "on"
+      }]
+    }, {
+      "featureType": "road.local",
+      "stylers": [{
+        "saturation": -100
+      }, {
+        "lightness": 40
+      }, {
+        "visibility": "on"
+      }]
+    }, {
+      "featureType": "transit",
+      "stylers": [{
+        "saturation": -100
+      }, {
+        "visibility": "simplified"
+      }]
+    }, {
+      "featureType": "administrative.province",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "water",
+      "elementType": "labels",
+      "stylers": [{
+        "visibility": "on"
+      }, {
+        "lightness": -25
+      }, {
+        "saturation": -100
+      }]
+    }, {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [{
+        "hue": "#ffff00"
+      }, {
+        "lightness": -25
+      }, {
+        "saturation": -97
+      }]
+    }]
+  // End map style
 
-// Begin map style: https://snazzymaps.com/
-[{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
-// End map style
-
-  var myLatlng = new google.maps.LatLng(-37.817251, 144.955891); // Set coordinates.
+  var myLatlng = new google.maps.LatLng(-6.932550, 107.776687); // Set coordinates.
   var mapOptions = {
     mapTypeControl: true,
     scrollwheel: false,
@@ -20,11 +94,11 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   // Marker
-  var iconBase = 'assets/img/';
+  var iconBase = '../assets/img/';
   var marker = new google.maps.Marker({
-      position: myLatlng,
-      title:"Hello! We Are Here. :)",
-      icon: iconBase + 'map-marker.png'
+    position: myLatlng,
+    title: "Hello! We Are Here. :)",
+    icon: iconBase + 'map-marker.png'
   });
 
   // To add the marker to the map, call setMap();
